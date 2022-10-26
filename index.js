c2d615b76cf994f0696b49e1e44bbe7d85681d17
yeasin62 <gazi.yeasin@yahoo.com>
2022-10-26 06:41:24 +0600

    created api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+const courses = require('./data/courses.json');
+const course = require('./data/course.json');
+
+app.get('/', (req, res)=> {
+    res.send('News API Running');
+});
+
+app.get('/courses',(req,res)=>{
+    res.send(courses);
+})
+
+app.get('/course/:id',(req,res)=>{
+    console.log(req.params.id);
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.course_id === id);
+    res.send(selectedCourse);
+})
+
+app.listen(port, ()=> {
+    console.log('Server run');
+})
