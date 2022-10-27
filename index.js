b247906981d95e35c695648ed02c708e7460a83d
yeasin62 <gazi.yeasin@yahoo.com>
2022-10-27 11:03:07 +0600

    url fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-//var cors = require('cors');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.use(cors());
+app.use(cors());
 
 
 const courses = require('./data/courses.json');
@@ -21,12 +21,7 @@ app.get('/all',(req,res)=>{
     res.send(course);
 })
 
-app.get('/course/:id',(req,res)=>{
-    console.log(req.params.id);
-    const id = req.params.id;
-    const selectedCourse = course.find(c => c.course_id === id);
-    res.send(selectedCourse);
-})
+
 
 app.listen(port, ()=> {
     console.log('Server run');
